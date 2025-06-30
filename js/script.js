8fb5af6ad1a68f019cb62b537f77967899e7addf
DagimDev <simedagim121@gmail.com>
2025-06-30 15:19:27 +0300

    Implement basic game outcome rules

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ const pickComputerMove = () => {
     ? "paper"
     : "scissors";
 };
+
+// basic game outcome logic
+
+const playGame = (playerMove) => {
+  let result = "";
+  const computerMove = pickComputerMove();
+
+  if (playerMove === "rock") {
+    if (computerMove === "rock") result = "Tie.";
+    else if (computerMove === "paper") result = "You lose.";
+    else if (computerMove === "scissors") result = "You win.";
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") result = "You win.";
+    else if (computerMove === "paper") result = "Tie.";
+    else if (computerMove === "scissors") result = "You lose.";
+  } else if (playerMove === "scissors") {
+    if (computerMove === "rock") result = "You lose.";
+    else if (computerMove === "paper") result = "You win.";
+    else if (computerMove === "scissors") result = "Tie.";
+  }
+  return result;
+};
